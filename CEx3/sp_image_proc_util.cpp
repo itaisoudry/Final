aaf5cc24380061cf9fceb54ee55aec78e0ce64ec
ifat <ifat@ubuntu>
2017-01-18 08:34:13 -0800

    ifat

--- CEx3/sp_image_proc_util.cpp
+++ CEx3/sp_image_proc_util.cpp
@@ -20,11 +20,14 @@
 #include <opencv2/features2d.hpp>
 
 #include <cstdio>
+#define NORMALIZE_FACTOR 0.33
+#define HIST_NUM 3
+#define PIXELS 256
 
 using namespace cv;
 SPPoint** spGetRGBHist(const char* str, int imageIndex, int nBins){
 		cv::Mat src;
-		SPPoint** result =(SPPoint**) malloc(3 * sizeof(SPPoint*));
+		SPPoint** result =(SPPoint**) malloc(HIST_NUM * sizeof(SPPoint*));
 		if (str == NULL || nBins <= 0 || imageIndex <= 0) {
 			printf(ERROR_GENERAL);
 			return NULL;
@@ -38,26 +41,42 @@ SPPoint** spGetRGBHist(const char* str, int imageIndex, int nBins){
 		//separate the img in 3 places (b,g,r)
 		std::vector<Mat> bgr_planes;
 		split(src,bgr_planes);
-		float range[] = {0, nBins};
+		float range[] = {0,PIXELS};
 		const float* histRange = {range};
 		Mat b_hist, g_hist, r_hist;
 		calcHist(&bgr_planes[0], 1, 0, Mat(),  b_hist, 1, &nBins, &histRange);
-		calcHist(&bgr_planes[1], 1, 0, Mat(),  b_hist, 1, &nBins, &histRange);
-		calcHist(&bgr_planes[2], 1, 0, Mat(),  b_hist, 1, &nBins, &histRange);
-		double data[nBins];
+		calcHist(&bgr_planes[1], 1, 0, Mat(),  g_hist, 1, &nBins, &histRange);
+		calcHist(&bgr_planes[2], 1, 0, Mat(),  r_hist, 1, &nBins, &histRange);
+		double* data = (double*)malloc(nBins*sizeof(double));
 		for (int i=0; i<nBins;i++){
 			data[i] = r_hist.at<double>(i);
 		}
 		result[0]= spPointCreate(data,nBins,imageIndex);
+		if (result[0]==NULL) {
+			free(result);
+			return NULL;
+		}
 		for (int i=0; i<nBins;i++){
 					data[i] = g_hist.at<double>(i);
 		}
 		result[1]= spPointCreate(data,nBins,imageIndex);
+		if (result[1]==NULL) {
+					free(result[0]);
+					free(result);
+					return NULL;
+				}
 
 		for (int i=0; i<nBins;i++){
 					data[i] = b_hist.at<double>(i);
 		}
 		result[2]= spPointCreate(data,nBins,imageIndex);
+		if (result[2]==NULL) {
+					free(result[0]);
+					free(result[1]);
+					free(result);
+					return NULL;
+				}
+		free(data);
 		return result;
 }
 double spRGBHistL2Distance(SPPoint** rgbHistA, SPPoint** rgbHistB){
@@ -68,7 +87,7 @@ double spRGBHistL2Distance(SPPoint** rgbHistA, SPPoint** rgbHistB){
 		sumG = spPointL2SquaredDistance(rgbHistA[1],rgbHistB[1]);
 		sumB = spPointL2SquaredDistance(rgbHistA[2],rgbHistB[2]);
 
-		result = 0.33*sumR +0.33*sumG + 0.33*sumB;
+		result = NORMALIZE_FACTOR*sumR +NORMALIZE_FACTOR*sumG + NORMALIZE_FACTOR*sumB;
 		return result;
 }
 
